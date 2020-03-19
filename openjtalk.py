17fe84cceabb962c97f3e67d3dcdc28788e96936
wgdp444 <tatsuyori.shiraga@triple-four.com>
2020-03-19 17:52:55 +0900

    相対パスを絶対パスに変更

--- openjtalk.py
+++ openjtalk.py
@@ -1,10 +1,12 @@
 import subprocess
 import tempfile
+import os
+current_path = os.getcwd()
 def yomiage(text):
     dict_path = '/var/lib/mecab/dic/open-jtalk/naist-jdic'
-    voice_path = './voice/mei_nomal.htsvoice'
+    voice_path = current_path + '/voice/mei_nomal.htsvoice'
     talk_speed = '1.0'
-    out_wav_name = './tmp/yomiage_tmp.wav'
+    out_wav_name = current_path + '/tmp/yomiage_tmp.wav'
     # make command
     generate_talk_wav_command = f'open_jtalk -x {dict_path} -m {voice_path} -r {talk_speed} -ow {out_wav_name}'
     result = subprocess.run([generate_talk_wav_command], stdin=subprocess.PIPE)
