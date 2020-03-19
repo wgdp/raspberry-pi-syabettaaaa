b199bcd20a5ecd334ec1973202904e91dc83dc44
wgdp444 <tatsuyori.shiraga@triple-four.com>
2020-03-19 17:37:11 +0900

    一時ファイルではなく標準入力に変更

--- openjtalk.py
+++ openjtalk.py
@@ -5,12 +5,11 @@ def yomiage(text):
     voice_path = './voice/mei_nomal.htsvoice'
     talk_speed = '1.0'
     out_wav_name = './tmp/yomiage_tmp.wav'
-    with tempfile.NamedTemporaryFile(mode='w+') as tmp:
-        tmp.write(text)
-        tmp.seek(0)
-        # make command
-        generate_talk_wav_command = f'open_jtalk -x {dict_path} -m {voice_path} -r {talk_speed} -ow {out_wav_name} {tmp.name}'
-        result = subprocess.run([generate_talk_wav_command])
+    # make command
+    generate_talk_wav_command = f'open_jtalk -x {dict_path} -m {voice_path} -r {talk_speed} -ow {out_wav_name}'
+    result = subprocess.run([generate_talk_wav_command], stdin=subprocess.PIPE)
+    result.stdin.write(text.encode())
+    result.stdin.close()
     yomiage_command = f'aplay -q {out_wav_name}'
     subprocess.run([yomiage_command])
     return True
